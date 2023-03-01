8ddb7db38ec2fab7d91a200f2dec33b9c69ce513
Elsa Camacho Luis <elsamonicatreze@gmail.com>
2023-03-01 10:57:31 +0000

    I added order ID tests and class, everything has passed, need to now work on the costumerID , order date, total amount and order status.

---
+++ Testing6/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing6
+{
+    public class OrderIdValidator
+    {
+        public bool IsValidOrderId(string orderId)
+        {
+            // Check if the order ID is in the correct format
+            if (orderId.Length != 8)
+            {
+                return false;
+            }
+
+            // Check if the order ID consists of only digits
+            foreach (char c in orderId)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            // Add any additional requirements for the order ID here
+            // ...
+
+            // If all checks pass, the order ID is valid
+            return true;
+        }
+    }
+
+}
---
+++ Testing6/TstPayment.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class TstPayment
+    {
+        [TestMethod]
+        public void TestValidOrderId()
+        {
+            // Arrange
+            string orderId = "12345678";
+
+            // Act
+            bool isValid = IsValidOrderId(orderId);
+
+            // Assert
+            Assert.IsTrue(isValid);
+        }
+
+        [TestMethod]
+        public void TestInvalidOrderId()
+        {
+            // Arrange
+            string orderId = "notanumber";
+
+            // Act
+            bool isValid = IsValidOrderId(orderId);
+
+            // Assert
+            Assert.IsFalse(isValid);
+        }
+
+        private bool IsValidOrderId(string orderId)
+        {
+            // Check if the order ID is in the correct format
+            if (orderId.Length != 8)
+            {
+                return false;
+            }
+
+            // Check if the order ID consists of only digits
+            foreach (char c in orderId)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+
+            // Add any additional requirements for the order ID here
+            // ...
+
+            // If all checks pass, the order ID is valid
+            return true;
+        }
+    }
+}
